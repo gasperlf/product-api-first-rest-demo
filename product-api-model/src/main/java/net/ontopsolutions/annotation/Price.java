2d2e7365d97e21c889eee650ed88d2dff816433e
lflorez <lflorez@paradigmadigital.com>
2024-11-22 13:30:34 +0100

    FEAT: Added more example endpoint for pagination

--- product-api-model/src/main/java/net/ontopsolutions/annotation/Price.java
+++ product-api-model/src/main/java/net/ontopsolutions/annotation/Price.java
@@ -1,11 +1,15 @@
 package net.ontopsolutions.annotation;
 
+import javax.validation.Payload;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+@Target({ ElementType.FIELD })
 @Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.TYPE)
-public @interface Age {
+public @interface Price {
+    String message() default "Invalid email address";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
 }
